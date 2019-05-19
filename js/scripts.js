d5956bc816dee161c4af6e4d1be1e70cfca4248d
DudeCalledZee <40342826+DudeCalledZee@users.noreply.github.com>
2019-05-19 16:48:21 +0100

    make game flip and match

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,40 @@
 
 const cards = document.querySelectorAll('.card');
 
+let isCardFlipped = false;
+let twoCardFlipped = false;
+let firstCard;
+let secondCard;
 
 // Loop through list add listener
 cards.forEach(card => card.addEventListener('click', cardFlip));
 
-
 // Card Flip Function
 function cardFlip() {
+  // stops more then two cards opening
+  if (twoCardFlipped) return;
+  // stops same card opening twice
+  if (this === firstCard) return;
+
   this.classList.toggle('flip');
+  //have two cards been flipped
+  if (!isCardFlipped) {
+    isCardFlipped = true;
+    firstCard = this;
+  } else {
+    isCardFlipped = false;
+    secondCard = this;
+    // is it a match
+    if (firstCard.dataset.name === secondCard.dataset.name) {
+      firstCard.removeEventListener('click', cardFlip);
+      secondCard.removeEventListener('click', cardFlip);
+    } else {
+      twoCardFlipped = true;
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        twoCardFlipped = false;
+      }, 1000);
+    }
+  }
 }
